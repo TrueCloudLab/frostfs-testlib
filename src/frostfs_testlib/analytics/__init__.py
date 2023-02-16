255c6b5eecdfee780a6b0108fddd1ef64e03286b
Aleksei Chetaev <alex.chetaev@gmail.com>
2023-02-16 17:23:29 +0100

    Fix issue in imports with analytics module

--- src/frostfs_testlib/analytics/__init__.py
+++ src/frostfs_testlib/analytics/__init__.py
@@ -1,4 +1,4 @@
 from test_case import id, suite_name, suite_section, title
 from test_collector import TestCase, TestCaseCollector
-from testrail_exporter import TestrailExporter
+from test_exporter import TestExporter
 from testrail_exporter import TestrailExporter
